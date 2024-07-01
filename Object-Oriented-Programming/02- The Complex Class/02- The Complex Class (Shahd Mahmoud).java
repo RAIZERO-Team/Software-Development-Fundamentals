4de07be81d87e0f6512b8379e708a24416ba2344
shahd <shahd.mahmoud1043@gmail.com>
2024-07-01 05:15:10 +0300

    Create second oop

--- Object-Oriented-Programming/02- The Complex Class/02- The Complex Class (Shahd Mahmoud).java
+++ Object-Oriented-Programming/02- The Complex Class/02- The Complex Class (Shahd Mahmoud).java
@@ -1,1 +1,66 @@
 //Author Shahd Mahmoud
+package taskoop2;
+
+public class Taskoop2 {
+
+    public static void main(String[] args) {
+        Complexnumbers c1 = new Complexnumbers(3, 4);
+        Complexnumbers c2 = new Complexnumbers(1, 2);
+
+        Complexnumbers c3 = c1.AddComplex(c2);
+
+        c3.PrintComplex();
+
+    }
+
+}
+
+class Complexnumbers {
+
+    private int real;
+    private int img;
+
+    public Complexnumbers(int real, int img) {
+        this.real = real;
+        this.img = img;
+
+    }
+
+    public void setReal(int real) {
+        this.real = real;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public float getReal() {
+        return real;
+    }
+
+    public float getImg() {
+        return img;
+    }
+
+    public Complexnumbers AddComplex(Complexnumbers c) {
+        int new_real = real + c.real;
+        int new_img = img + c.img;
+        c = new Complexnumbers(new_real, new_img);
+
+        return c;
+    }
+
+    public Complexnumbers SubComplex(Complexnumbers c) {
+        int new_real = real - c.real;
+        int new_img = img - c.img;
+
+        c = new Complexnumbers(new_real, new_img);
+        return c;
+    }
+
+    public void PrintComplex() {
+
+        System.out.println(real + " + " + img + "i");
+    }
+
+}
