b73af8e475d0b3a0c652b83ffc553fc18dad44a7
ma7er <ahmedbnmaher1@gmail.com>
2024-07-19 03:36:12 +0300

    solve task 17 in OOP

--- Object-Oriented-Programming/17- Read and Write to a File/17- Read and Write to a File (Ahmed Maher)/17- Read and Write to a File (Ahmed Maher).py
+++ Object-Oriented-Programming/17- Read and Write to a File/17- Read and Write to a File (Ahmed Maher)/17- Read and Write to a File (Ahmed Maher).py
@@ -1,16 +1,16 @@
 # Author: Ahmed Maher
 
-input_file = r'F:\d\Github\Software\Software-Development-Fundamentals\Object-Oriented-Programming\17- Read and Write to a File\17- Read and Write to a File (Ahmed Maher)\Ma7er.txt'
+inputFile = r'F:\d\Github\Software\Software-Development-Fundamentals\Object-Oriented-Programming\17- Read and Write to a File\17- Read and Write to a File (Ahmed Maher)\Ma7er.txt'
 
 try:
-    with open(input_file, 'r') as file:
+    with open(inputFile, 'r') as file:
         content = file.read()
         print(content)
 except Exception as e:
     print(f"An error in reading the file: {e}")
 
 try:
-    with open(input_file, 'a') as file:
+    with open(inputFile, 'a') as file:
         file.write("\nthis is new line")
 except Exception as e:
     print(f"An error in writing to the file: {e}")
