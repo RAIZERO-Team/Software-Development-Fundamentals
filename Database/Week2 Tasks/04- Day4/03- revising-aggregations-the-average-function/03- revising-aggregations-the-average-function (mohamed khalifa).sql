8977feacf2606deb925f03c6e278592a6625bfa4
Mohamed <mohammedelkady8889@gmail.com>
2024-08-08 09:28:54 +0300

    upload solve Day 4

---
+++ Database/Week2 Tasks/04- Day4/03- revising-aggregations-the-average-function/03- revising-aggregations-the-average-function (mohamed khalifa).sql
@@ -0,0 +1,3 @@
+-- Author mohamed khalifa 
+
+select AVG(population) from city where District = "California"
---
+++ Database/Week2 Tasks/04- Day4/05- weather-observation-station-3/05- weather-observation-station-3 (mohamed khalifa).sql
@@ -0,0 +1,3 @@
+-- Author mohamed khalifa 
+
+select distinct  city  from station where id%2 = 0
