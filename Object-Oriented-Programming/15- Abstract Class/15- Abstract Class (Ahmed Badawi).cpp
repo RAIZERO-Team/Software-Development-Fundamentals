254bd4feff57265d47a3636ff51326889646d70b
ahmed badawi <ahmedbad063@gmail.com>
2024-08-12 09:18:52 +0300

    upload my solve

--- Object-Oriented-Programming/15- Abstract Class/15- Abstract Class (Ahmed Badawi).cpp
+++ Object-Oriented-Programming/15- Abstract Class/15- Abstract Class (Ahmed Badawi).cpp
@@ -1,9 +1,66 @@
 #include <iostream>
 using namespace std;
 
+class Account {
+public:
+  double balance;
+  string accountNumber;
 
+  Account() : balance(0), accountNumber("") {}
+
+  virtual double calculateInterest() = 0;
+  double deposit(double amount) {
+    return balance += amount;
+  }
+
+  double withdraw(double amount) {
+    return balance -= amount;
+  }
+};
+
+class SavingsAccount : public Account {
+public:
+  SavingsAccount(double balance, string accountNumber) {
+    this -> balance = balance;
+    this -> accountNumber = accountNumber;
+  }
+
+  double calculateInterest(){
+    return (this -> balance * 3) / 100;
+  }
+};
+
+class CheckingAccount : public Account {
+public:
+  CheckingAccount(double balance, string accountNumber) {
+    this -> balance = balance;
+    this -> accountNumber = accountNumber;
+  }
+
+  double calculateInterest(){
+    return (this -> balance * 1) / 100;
+  }
+};
 
 int main(){
+  SavingsAccount SavingsAccount1(5000, "5624-4956-7551-4625");
+  CheckingAccount checkingAccount1(3000, "1597-4658-8921-1568");
+
+  cout << "Welcome to Saving Account\n";
+  cout << "Account Number: " << SavingsAccount1.accountNumber << "\n"
+      << "Balance: " << SavingsAccount1.balance << "\n"
+      << "Deposit 50$: " << SavingsAccount1.deposit(50) << "$" << "\n"
+      << "Withdraw 100$: " << SavingsAccount1.withdraw(100) << "$" << "\n"
+      << "Interest Account Per Year: " << SavingsAccount1.calculateInterest() << " $";
+
+  cout << "\n******************\n";
+
+  cout << "Welcome to Checking Account\n";
+  cout << "Account Number: " << checkingAccount1.accountNumber << "\n"
+      << "Balance: " << checkingAccount1.balance << "\n"
+      << "Deposit 100$: " << checkingAccount1.deposit(100) << "$" << "\n"
+      << "Withdraw 200$: " << checkingAccount1.withdraw(200) << "$" << "\n"
+      << "Interest Account Per Year: " << checkingAccount1.calculateInterest() << "$";
 
   return 0;
 }
---
+++ Object-Oriented-Programming/18- Connection to DataBase/18- Connection to DataBase (Ahmed Badawi)/18- Connection to DataBase (Ahmed Badawi).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <mysqlStubs.h>
+using namespace std;
+
+struct connection {
+  const char *server, *user, *password, *database;
+};
+
+int main(){
+
+  return 0;
+}
